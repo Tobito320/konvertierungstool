cf7ae27dbe8a2868e57e9df46f70cdb448e5dce2
Tobito320 <ahmedhdplay12345@gmail.com>
2025-07-07 23:10:31 +0200

    first commit huh

---
+++ binaerwandler.py
@@ -0,0 +1,12 @@
+# Projekt: Konvertierungs-Tool
+# Name: Ahmed Abou Dmag
+# Klasse: IT24B
+
+# Module:
+# 1: Binär zu Dezimal
+# 2: Binär zu Hexadezimal
+# 3: Dezimal zu Binär
+# 4: Dezimal zu Hexadezimal
+# 5: Hexadezimal zu Binär
+# 6: Hexadezimal zu Dezimal
+# 7: Programm beenden
